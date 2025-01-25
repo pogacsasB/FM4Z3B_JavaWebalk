ecc0d11e6850e75c7bc90c5d522524f250b73658
pogacsasB <91243056+pogacsasB@users.noreply.github.com>
2025-01-25 19:23:05 +0100

    Add files via upload

---
+++ fm4z3b_java_webalk/src/main/java/com/pogb/fm4z3b_java_webalk/Fm4z3bJavaWebalkApplication.java
@@ -0,0 +1,13 @@
+package com.pogb.fm4z3b_java_webalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fm4z3bJavaWebalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fm4z3bJavaWebalkApplication.class, args);
+	}
+
+}
