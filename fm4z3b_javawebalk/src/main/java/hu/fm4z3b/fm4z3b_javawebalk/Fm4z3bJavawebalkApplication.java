278581adc4806aa0ebedeea06aed6fcc94b0edb3
pogacsasB <91243056+pogacsasB@users.noreply.github.com>
2025-01-25 20:27:30 +0100

    Add files via upload

---
+++ fm4z3b_javawebalk/src/main/java/hu/fm4z3b/fm4z3b_javawebalk/Fm4z3bJavawebalkApplication.java
@@ -0,0 +1,13 @@
+package hu.fm4z3b.fm4z3b_javawebalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fm4z3bJavawebalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fm4z3bJavawebalkApplication.class, args);
+    }
+
+}
---
+++ fm4z3b_javawebalk/src/test/java/hu/fm4z3b/fm4z3b_javawebalk/Fm4z3bJavawebalkApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.fm4z3b.fm4z3b_javawebalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fm4z3bJavawebalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
