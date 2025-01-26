1502bbe05b7070d357211fcfc84b1a1e7ce77e5b
pogacsasb <pogacsasb@gmail.com>
2025-01-26 20:51:44 +0100

    commit

--- java_webalk/src/main/java/hu/fm4z3b/java_webalk/controllers/HomeController.java
+++ java_webalk/src/main/java/hu/fm4z3b/java_webalk/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package hu.fm4z3b.java_webalk.controller;
+package hu.fm4z3b.java_webalk.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
