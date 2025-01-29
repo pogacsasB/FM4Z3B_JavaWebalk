1c514d43174bc77101e0c4a877eee80005eecd70
pogacsasB <91243056+pogacsasB@users.noreply.github.com>
2025-01-29 21:04:43 +0100

    Delete java_webalk directory

--- java_webalk/src/main/java/hu/fm4z3b/java_webalk/services/HouseRepository.java
+++
@@ -1,8 +0,0 @@
-package hu.fm4z3b.java_webalk.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import hu.fm4z3b.java_webalk.models.House;
-
-public interface HouseRepository extends JpaRepository<House, Integer> {
-
-}
--- java_webalk/src/main/java/hu/fm4z3b/java_webalk/services/PeopleRepository.java
+++
@@ -1,8 +0,0 @@
-package hu.fm4z3b.java_webalk.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import hu.fm4z3b.java_webalk.models.People;
-
-public interface PeopleRepository extends JpaRepository<People, Integer> {
-    
-}
